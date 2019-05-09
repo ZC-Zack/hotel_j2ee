b2ffe33b16f035ed9eef765a014a0ce56466139b
沙漏的凝滞 <1301842136@qq.com>
2019-05-10 00:20:04 +0800

    Initial commit

---
+++ src/main/java/com/xmut/hotel/HotelApplication.java
@@ -0,0 +1,14 @@
+package com.xmut.hotel;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+
+}
