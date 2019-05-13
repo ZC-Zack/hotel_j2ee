e811a54d76a6b3061e72fcb7cf4c6d1b58a5fe3c
沙漏的凝滞 <1301842136@qq.com>
2019-05-14 01:46:01 +0800

    新增员工表

---
+++ src/main/java/com/xmut/hotel/entity/Employee.java
@@ -0,0 +1,53 @@
+package com.xmut.hotel.entity;
+
+public class Employee {
+    private String employeeId;
+
+    private String name;
+
+    private String sex;
+
+    private String telephone;
+
+    private Integer salary;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId == null ? null : employeeId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
