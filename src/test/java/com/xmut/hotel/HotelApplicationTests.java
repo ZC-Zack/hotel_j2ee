b950ed4f71ea2b400163eb1d575aa34b42df8534
沙漏的凝滞 <1301842136@qq.com>
2019-05-16 00:35:55 +0800

    新增房间预订简单样式

--- src/test/java/com/xmut/hotel/HotelApplicationTests.java
+++ src/test/java/com/xmut/hotel/HotelApplicationTests.java
@@ -5,12 +5,22 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HotelApplicationTests {
 
     @Test
     public void contextLoads() {
+        String dataString = "2018-09-11";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            System.out.println(sdf.parse(dataString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
